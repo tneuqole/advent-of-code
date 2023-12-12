f84cbb48355123857c65ad98969b24ec86622aa1
tneuqole <woodmanjd32@gmail.com>
2023-12-12 10:28:58 -0500

    day 4 part 2

--- src/day4/day4.go
+++ src/day4/day4.go
@@ -26,7 +26,7 @@ func p1() {
         s := scanner.Text()
         raw := strings.Split(s, ":")
         raw = strings.Split(raw[1], "|")
-
+        
         winningNums := strings.Fields(raw[0])
         card := strings.Fields(raw[1])
         
@@ -45,7 +45,61 @@ func p1() {
     fmt.Println("problem 1: ", sum)
 }
 
+type card struct {
+    winningNums []string
+    nums []string
+}
+
+var cards []card
+
+func parseCards() {
+    file, err := os.Open("day4.txt")
+    check(err)
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        s := scanner.Text()
+        raw := strings.Split(s, ":")
+        raw = strings.Split(raw[1], "|")
+
+        c := card {
+            winningNums: strings.Fields(raw[0]),
+            nums: strings.Fields(raw[1]),
+        }
+        cards = append(cards, c)
+    }
+}
+
+func countCopies(idx int) int {
+    copies := 1
+    c := cards[idx]
+    fmt.Println("Processing card: ", idx+1)
+    count := 0
+    for _, n := range c.winningNums {
+        if slices.Contains(c.nums, n) {
+            count++
+        }
+    }
+
+    for j := 0; j < count; j++ {
+        copies += countCopies(j+1+idx)
+    }
+    return copies
+}
+
+func p2() {
+    parseCards()
+    var copies int
+    for i := 0; i < len(cards); i++ {
+        copies += countCopies(i)
+    }
+
+    fmt.Println("problem 2: ", copies)
+}
+
 func main() {
     p1()
+    p2()
 }
 
