e870bd9d01699a59dcf6c4365b5f8cb928b6952a
tneuqole <woodmanjd32@gmail.com>
2024-11-20 21:26:39 -0500

    solve day 25

---
+++ 2023/python/day25.py
@@ -0,0 +1,56 @@
+import random
+from collections import defaultdict
+from copy import deepcopy
+from datetime import datetime
+from pprint import pprint
+
+from input import input, test_input  # local file, not committed
+
+
+def kargers(graph):
+    while len(graph) > 2:
+        v1 = random.choice(list(graph.keys()))
+        v2 = random.choice(graph[v1])
+
+        new_v = f"{v1}-{v2}"
+
+        graph[v1].extend(graph[v2])
+        graph[new_v] = [n for n in graph[v1] if n not in [v1, v2]]
+
+        del graph[v1]
+        del graph[v2]
+
+        for v, edges in graph.items():
+            graph[v] = [e if e not in [v1, v2] else new_v for e in edges]
+
+    return graph
+
+
+graph = defaultdict(list)
+for line in input.splitlines():
+    if not line:
+        continue
+
+    v, conns = line.split(":")
+    conns = conns.strip(" ").split(" ")
+
+    for conn in conns:
+        if conn not in graph[v]:
+            graph[v].append(conn)
+        if v not in graph[conn]:
+            graph[conn].append(v)
+
+attempts = 0
+start = datetime.now()
+while True:
+    attempts += 1
+    result = kargers(deepcopy(graph))
+    v, e = result.popitem()
+    if not len(e) == 3:
+        continue
+
+    ans = len(v.split("-")) * len(e[0].split("-"))
+    print(f"attempts={attempts}, ans={ans}")
+    break
+
+print(f"took={datetime.now() - start}")
