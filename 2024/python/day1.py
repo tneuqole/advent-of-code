76306071fdab4c7f67a9c4cbfd97f43abab5c109
tneuqole <woodmanjd32@gmail.com>
2024-12-01 00:15:35 -0500

    2024 DAY 1 :D

---
+++ 2024/python/day1.py
@@ -0,0 +1,25 @@
+import functools
+import itertools
+from collections import defaultdict, deque
+from copy import copy, deepcopy
+from datetime import datetime
+from pprint import pprint
+
+start = datetime.now()
+
+input = open(0).read().splitlines()
+
+list1, list2 = [], []
+for line in input:
+    ints = line.split("   ")
+    list1.append(int(ints[0]))
+    list2.append(int(ints[1]))
+
+list1 = sorted(list1)
+list2 = sorted(list2)
+
+print(sum(abs(list1[i] - list2[i]) for i in range(len(list1))))
+
+print(sum(list1[i] * list2.count(list1[i]) for i in range(len(list1))))
+
+print(f"took={datetime.now() - start}")
