be01c748359dd8a415046477825cbadc4600b755
tneuqole <woodmanjd32@gmail.com>
2024-12-03 23:12:39 -0500

    test file

---
+++ 2019/test_intcode.py
@@ -0,0 +1,14 @@
+import pytest
+from day2 import day2p1, day2p2
+from day5 import day5p1, day5p2
+
+
+def test_run(capsys):
+    assert day2p1() == 8017076
+    assert day2p2() == 3146
+
+    day5p1()
+    assert "13210611" in capsys.readouterr().out
+
+    day5p2()
+    assert "584126" in capsys.readouterr().out
