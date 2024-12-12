e3ce1b80ac58818acc56f96bd9b1c367a7b4e053
tneuqole <woodmanjd32@gmail.com>
2024-12-12 12:18:23 -0500

    cleanup

--- 2024/day12.py
+++ 2024/day12.py
@@ -1,11 +1,10 @@
-from collections import defaultdict, deque
+from collections import deque
 from datetime import datetime
 
 start = datetime.now()
 
 grid = open(0).read().splitlines()
 
-coords = defaultdict(list)
 
 DIRS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
@@ -44,16 +43,16 @@ for r in range(len(grid)):
             continue
         visited = bfs(grid, (r, c))
         seen.update(visited)
-        plants.append((grid[r][c], visited))
+        plants.append(visited)
 
 
 ans1, ans2 = 0, 0
-for a, cs in plants:
-    p = []
-    pc = []
-    sides = 0
-    for r, c in cs:
-        p.extend(
+for coords in plants:
+    perim = []
+    corners = 0
+    for r, c in coords:
+        # add all sides
+        perim.extend(
             [
                 (r - 1, c),
                 (r, c + 1),
@@ -65,40 +64,57 @@ for a, cs in plants:
         # find number of corners, inner and outer
         # outer
         # TL
-        if (r - 1, c) not in cs and (r, c - 1) not in cs:
-            sides += 1
+        if (r - 1, c) not in coords and (r, c - 1) not in coords:
+            corners += 1
 
         # TR
-        if (r - 1, c) not in cs and (r, c + 1) not in cs:
-            sides += 1
+        if (r - 1, c) not in coords and (r, c + 1) not in coords:
+            corners += 1
 
         # BL
-        if (r + 1, c) not in cs and (r, c - 1) not in cs:
-            sides += 1
+        if (r + 1, c) not in coords and (r, c - 1) not in coords:
+            corners += 1
 
         # BR
-        if (r + 1, c) not in cs and (r, c + 1) not in cs:
-            sides += 1
+        if (r + 1, c) not in coords and (r, c + 1) not in coords:
+            corners += 1
 
         # inside
         # TL
-        if (r - 1, c) in cs and (r, c - 1) in cs and (r - 1, c - 1) not in cs:
-            sides += 1
+        if (
+            (r - 1, c) in coords
+            and (r, c - 1) in coords
+            and (r - 1, c - 1) not in coords
+        ):
+            corners += 1
 
         # TR
-        if (r - 1, c) in cs and (r, c + 1) in cs and (r - 1, c + 1) not in cs:
-            sides += 1
+        if (
+            (r - 1, c) in coords
+            and (r, c + 1) in coords
+            and (r - 1, c + 1) not in coords
+        ):
+            corners += 1
 
         # BL
-        if (r + 1, c) in cs and (r, c - 1) in cs and (r + 1, c - 1) not in cs:
-            sides += 1
+        if (
+            (r + 1, c) in coords
+            and (r, c - 1) in coords
+            and (r + 1, c - 1) not in coords
+        ):
+            corners += 1
 
         # BR
-        if (r + 1, c) in cs and (r, c + 1) in cs and (r + 1, c + 1) not in cs:
-            sides += 1
+        if (
+            (r + 1, c) in coords
+            and (r, c + 1) in coords
+            and (r + 1, c + 1) not in coords
+        ):
+            corners += 1
 
-    ans1 += len([c for c in p if c not in cs]) * len(cs)
-    ans2 += sides * len(cs)
+    # remove coords that are a plant
+    ans1 += len([c for c in perim if c not in coords]) * len(coords)
+    ans2 += corners * len(coords)
 
 print(ans1)
 print(ans2)
