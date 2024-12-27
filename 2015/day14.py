81f57c9b810a6e6026dbd1559689f2ea9b692b19
tneuqole <woodmanjd32@gmail.com>
2024-12-27 13:56:57 -0500

    cleanup

--- 2015/day14.py
+++ 2015/day14.py
@@ -17,27 +17,11 @@ class Reindeer:
         self.dist = 0
 
 
-R = []
-dist = 0
-for row in data:
-    parts = row.split(" ")
-
-    id, v, t, w = parts[0], int(parts[3]), int(parts[6]), int(parts[13])
-    R.append(Reindeer(id, v, t, w))
-
-    flown = 0
-    lim = 2503
-    while lim > 0:
-        if lim > t:
-            lim -= t + w
-            flown += t * v
-        else:
-            flown += lim * t
-            lim -= t
-
-    dist = max(dist, flown)
-
-print(dist)
+R = list(
+    Reindeer(parts[0], int(parts[3]), int(parts[6]), int(parts[13]))
+    for row in data
+    for parts in [row.split(" ")]
+)
 
 for _ in range(2503):
     for r in R:
@@ -54,6 +38,7 @@ for _ in range(2503):
         if r.dist == d:
             r.points += 1
 
+print(max(R, key=lambda r: r.dist).dist)
 print(max(R, key=lambda r: r.points).points)
 
 print(f"took={datetime.now() - start}")
